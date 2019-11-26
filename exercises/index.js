0438ee4b883223271fb6b48967e32e021fb07a3e
Strange-Err <57707199+Strange-Err@users.noreply.github.com>
2019-11-27 07:46:14 +0800

    Update index.js

--- exercises/index.js
+++ exercises/index.js
@@ -1,4 +1,4 @@
-/* var exercisesPage = [
+var exercisesPage = [
     {
       question: "1.  What is the sample space for choosing an odd number from 1 to 11 at random?",
       answers: {
@@ -120,55 +120,4 @@ function generateQuiz (exercisesPage, quizContainer, resultsContainer, submitBut
 	} //results function
 	
 	
-} */
-
-var pos = 0, quiz, question, choice, choices, chA, chB, chC, chD, correctAns = 0;
-var questions = [
-	["what is 10 + 4?", "1", "2", "3", "14", "D"],
-	["what is 5 + 1?", "14", "13", "6", "5", "C"],
-	["what is 1 + 2?", "3", "5", "1", "2", "A"],
-	["what is 5 + 5?", "9", "10", "11", "12", "B"],
-	["what is 3 + 2?", "6", "4", "5", "1", "C"],
-];
-
-function _(x)
-{
-	return document.getElementById(x);
 }
-
-function renderQuestion()
-{
-	quiz = _("quiz");
-	if(pos >= questions.length){
-		quiz.innerHTML = "<h2>You got "+correct+" of "+questions.length+" questions correct</h2>";
-		pos = 0;
-		correct = 0;
-		return false;
-	}
-	question = questions[pos][0];
-	chA = questions[pos][1];
-	chB = questions[pos][2];
-	chC = questions[pos][3];
-	quiz.innerHTML = "<h3>"+question+"</h3>";
-	quiz.innerHTML += "<input type='radio' name='choices' value='A'> "+chA+"<br>";
-	quiz.innerHTML += "<input type='radio' name='choices' value='B'> "+chB+"<br>";
-	quiz.innerHTML += "<input type='radio' name='choices' value='C'> "+chC+"<br><br>";
-	quiz.innerHTML += "<button onclick='checkAnswer()'>Submit Answer</button>";
-	
-}
-
-function checkAnswer(){
-	choices = document.getElementsByName("choices");
-	for(var i=0; i<choices.length; i++){
-		if(choices[i].checked){
-			choice = choices[i].value;
-		}
-	}
-	if(choice == questions[pos][4]){
-		correct++;
-	}
-	pos++;
-	renderQuestion();
-}
-
-window.addEventListener("load", renderQuestion, false);
