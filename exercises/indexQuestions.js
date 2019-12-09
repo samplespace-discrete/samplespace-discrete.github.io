39eb8543e15ce4987082ccb6bff126d5c0579117
Strange-Err <57707199+Strange-Err@users.noreply.github.com>
2019-12-09 13:08:48 +0800

    Update indexQuestions.js

--- exercises/indexQuestions.js
+++ exercises/indexQuestions.js
@@ -1,6 +1,6 @@
 var questions = [
     {
-      q: "1.  What is the sample space for choosing an odd number from 1 to 11 at random?",
+      q: "What is the sample space for choosing an odd number from 1 to 11 at random?",
       o: [
          "1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11",
          "{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}",
@@ -10,7 +10,7 @@ var questions = [
       a: 2
    },
    {
-      q: "2.  What is the sample space for choosing a prime number less than 15 at random?",
+      q: "What is the sample space for choosing a prime number less than 15 at random?",
       o: [
          "{2, 3, 5, 7, 11, 13, 15}",
          "{2, 3, 5, 7, 11, 13}",
@@ -20,7 +20,7 @@ var questions = [
       a: 2
    },
    {
-      q: "3.  What is the sample space for choosing 1 jelly bean at random from a jar containing 5 red, 7 blue and 2 green jelly beans?",
+      q: "What is the sample space for choosing 1 jelly bean at random from a jar containing 5 red, 7 blue and 2 green jelly beans?",
       o: [
         "{5, 7, 2}",
         "{5 red, 7 blue, 2 green}",
@@ -30,7 +30,7 @@ var questions = [
       a: 3
    },
    {
-      q: "4.  What is the sample space for choosing 1 letter at random from 5 vowels?",
+      q: "What is the sample space for choosing 1 letter at random from 5 vowels?",
       o: [
         "{a, e, i, o, u}",
         "{v, o, w, e, l}",
@@ -40,7 +40,7 @@ var questions = [
       a: 0
    },
    {
-      q: "5.  What is the sample space for choosing 1 letter at random from the word DIVIDE?",
+      q: "What is the sample space for choosing 1 letter at random from the word DIVIDE?",
       o: [
         "{d, i, v, i, d, e}",
         "{1, 2, 3, 4, 5, 6}",
