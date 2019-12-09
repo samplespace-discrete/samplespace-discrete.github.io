efaf651b81bf6164778e273a6c78207236c05130
Strange-Err <57707199+Strange-Err@users.noreply.github.com>
2019-12-09 12:26:28 +0800

    Create indexQuestions.js

---
+++ exercises/indexQuestions.js
@@ -0,0 +1,52 @@
+var questions = [
+    {
+      q: "1.  What is the sample space for choosing an odd number from 1 to 11 at random?",
+      o: [
+         "1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11",
+         "{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}",
+         "{1, 3, 5, 7, 9 11}",
+         "None of the above"
+      ],
+      a: 2
+   },
+   {
+      q: "2.  What is the sample space for choosing a prime number less than 15 at random?",
+      o: [
+         "{2, 3, 5, 7, 11, 13, 15}",
+         "{2, 3, 5, 7, 11, 13}",
+         "{2, 3, 5, 7, 9, 11, 13}",
+         "All of the above"
+      ],
+      a: 2
+   },
+   {
+      q: "3.  What is the sample space for choosing 1 jelly bean at random from a jar containing 5 red, 7 blue and 2 green jelly beans?",
+      o: [
+        "{5, 7, 2}",
+        "{5 red, 7 blue, 2 green}",
+        "{red, blue, green}",
+        "None of the above"
+      ],
+      a: 3
+   },
+   {
+      q: "4.  What is the sample space for choosing 1 letter at random from 5 vowels?",
+      o: [
+        "{a, e, i, o, u}",
+        "{v, o, w, e, l}",
+        "{1, 2, 3, 4, 5}",
+        "None of the above"
+      ],
+      a: 0
+   },
+   {
+      q: "5.  What is the sample space for choosing 1 letter at random from the word DIVIDE?",
+      o: [
+        "{d, i, v, i, d, e}",
+        "{1, 2, 3, 4, 5, 6}",
+        "{d, i, v, e}",
+        "None of the above",
+      ],
+      a: 0
+   } 
+];
